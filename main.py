28c18f202079a01ea5a48734c933632fb4794776
user <adasdasd@adsa.dfgh>
2023-02-02 10:13:38 +0300

    b

--- main.py
+++ main.py
@@ -57,19 +57,19 @@ def fourth_task():
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
                     crossorigin="anonymous">
-                    <div class="alert alert-primary" role="alert">
+                    <div class="alert alert-dark" role="alert">
                       Человечество вырастает из детства.
                     </div>
-                    <div class="alert alert-secondary" role="alert">
+                    <div class="alert alert-success" role="alert">
                       Человечеству мала одна планета.
                     </div>
-                    <div class="alert alert-success" role="alert">
+                    <div class="alert alert-light" role="alert">
                       Мы сделаем обитаемыми безжизненные пока планеты.
                     </div>
-                    <div class="alert alert-danger" role="alert">
+                    <div class="alert alert-warning" role="alert">
                       И начнем с Марса!
                     </div>
-                    <div class="alert alert-warning" role="alert">
+                    <div class="alert alert-danger" role="alert">
                       Присоединяйся!
                     </div>
                   </body>
